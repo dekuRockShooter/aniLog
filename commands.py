7fabbfa7dc55b9c310e053ecf335a9b6ca74f6d7
dekuRockShooter <dekuRockShooter@gmail.com>
2015-12-21 09:07:01 -0800

    First commit.

---
+++ commands.py
@@ -0,0 +1,15 @@
+import curses
+
+class Command:
+    def __init__(self, name, desc, quantifier=1, kwargs**):
+        self._name = name
+        self._desc = desc
+        self._quantifier = quantifier
+        self._args = kwargs
+
+    def execute():
+        pass
+
+class ScrollDown(Command):
+    def execute():
+        pass

1aba4fa7b205aec0eea1eff44bba46d1b14a867f
dekuRockShooter <dekuRockShooter@gmail.com>
2015-12-21 17:43:38 -0800

    Add scroll up and down commands.

--- commands.py
+++ commands.py
@@ -1,15 +1,23 @@
 import curses
+from browser import Browser
+from shared import BrowserFactory
 
 class Command:
-    def __init__(self, name, desc, quantifier=1, kwargs**):
+    def __init__(self, name, desc, quantifier=1, **kwargs):
         self._name = name
         self._desc = desc
         self._quantifier = quantifier
         self._args = kwargs
 
-    def execute():
+    def execute(self):
         pass
 
 class ScrollDown(Command):
-    def execute():
-        pass
+    def execute(self):
+        cur_browser = BrowserFactory.get_cur()
+        cur_browser.scroll(Browser.DOWN, self._quantifier)
+
+class ScrollUp(Command):
+    def execute(self):
+        cur_browser = BrowserFactory.get_cur()
+        cur_browser.scroll(Browser.UP, self._quantifier)

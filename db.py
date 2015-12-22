674e8df59100dbd4bf440f79a5fe06c5c4f6b42f
dekuRockShooter <dekuRockShooter@gmail.com>
2015-12-22 11:40:40 -0800

    Create exception for accessing an unconnected database.

--- db.py
+++ db.py
@@ -1,11 +1,20 @@
 import os
 import sqlite3
 
+class NoConnectionError(Exception):
+    def __init__(self, value):
+        self._value = value
+
+    def __str__(self):
+        return repr(self._value)
+
 class DBConnection:
     def __init__(self, name):
         self._name = name
         self._connection = None
         self._cursor = None
+        self._no_connect_err = NoConnectionError(\
+                'Database not connected. Call connect().')
 
     def connect(self):
         if not os.path.exists(self._name):
@@ -18,6 +27,8 @@ class DBConnection:
             self._connection.close()
 
     def select_all_from(self, table):
+        if not self._connection:
+            raise self._no_connect_err
         self._cursor.execute('select * from {}'.format(table))
         return self._cursor.fetchall()
 

99605d39b131f59bea5815514fb137f3a8e41887
dekuRockShooter <dekuRockShooter@gmail.com>
2015-12-22 11:47:37 -0800

    Create a database registry and improve the browser registry.
    The browser registry now has destroy and create functions, just like the
    database registry.

--- shared.py
+++ shared.py
@@ -1,7 +1,9 @@
-from browser import Browser
+import db
+import browser
+#from browser import Browser
 
 class BrowserFactory:
-    _browser_list = {}
+    _browser_map = {}
     _cur_browser = None
 
     @staticmethod
@@ -10,22 +12,53 @@ class BrowserFactory:
 
     @staticmethod
     def set_cur(idx):
-        try:
-            BrowserFactory._cur_browser = BrowserFactory._browser_list[idx]
-        except KeyError:
-            pass
+        """Raises a KeyError"""
+        BrowserFactory._cur_browser = BrowserFactory._browser_map[idx]
 
     @staticmethod
-    def create_browser(name, scr_top_row, scr_left_col,\
-            scr_bot_row, scr_right_col):
-        if name in BrowserFactory._browser_list:
+    def create(scr_top_row, scr_left_col,\
+            scr_bot_row, scr_right_col, db_name, table):
+        name = '{}.{}'.format(db_name, table)
+        if name in BrowserFactory._browser_map:
             return
-        BrowserFactory._browser_list[name] =\
-                Browser(scr_top_row, scr_left_col, scr_bot_row, scr_right_col)
+        BrowserFactory._browser_map[name] =\
+                browser.Browser(scr_top_row, scr_left_col, scr_bot_row,\
+                scr_right_col, db_name, table)
 
     @staticmethod
-    def delete_browser(name):
-        try:
-            del BrowserFactory,_browser_list[name]
-        except KeyError:
-            pass
+    def destroy(name):
+        """Raises a KeyError"""
+        BrowserFactory,_browser_map[name].destroy()
+        BrowserFactory._browser_map.pop(name)
+
+    @staticmethod
+    def destroy_all():
+        for name, browser in BrowserFactory._browser_map.items():
+            browser.destroy()
+        BrowserFactory._browser_map.clear()
+
+class DBRegistry:
+    _db_map = {}
+
+    @staticmethod
+    def create(name):
+        if name in DBRegistry._db_map:
+            return
+        DBRegistry._db_map[name] = db.DBConnection(name)
+
+    @staticmethod
+    def get_db(name):
+        """Raises a KeyError"""
+        return DBRegistry._db_map[name]
+
+    @staticmethod
+    def destroy(name):
+        """Raises a KeyError"""
+        DBRegistry._db_map[name].close()
+        del DBRegistry._db_map[name]
+
+    @staticmethod
+    def destroy_all():
+        for db in DBRegistry._db_map.values():
+            db.close()
+        DBRegistry._db_map.clear()

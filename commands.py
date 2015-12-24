ffb9ef6b19bf6d43504b2c0177ceb00de29893eb
dekuRockShooter <dekuRockShooter@gmail.com>
2015-12-24 09:28:42 -0800

    Start a very, very rough draft of EditCell.

--- commands.py
+++ commands.py
@@ -1,6 +1,6 @@
 import curses
 from browser import Browser
-from shared import BrowserFactory
+from shared import BrowserFactory, StatusBarRegistry, DBRegistry
 
 class Command:
     def __init__(self, name, desc, quantifier=1, **kwargs):
@@ -21,3 +21,26 @@ class ScrollUp(Command):
     def execute(self):
         cur_browser = BrowserFactory.get_cur()
         cur_browser.scroll(Browser.UP, self._quantifier)
+
+class EditCell(Command):
+    def execute(self):
+        cur_browser = BrowserFactory.get_cur()
+        cur_status_bar = StatusBarRegistry.get()
+        cur_status_bar.set_str(str(cur_browser.get_cur_cell()))
+        cmd = cur_status_bar.redraw()
+        if len(cmd) != 3 or (cmd[1] == cmd[2]):
+            return
+        browser_name = cur_browser.get_name()
+        db_name = browser_name[: browser_name.rfind('.')]
+        table_name = browser_name[browser_name.rfind('.') + 1:]
+        cur_db = DBRegistry.get_db(db_name)
+        s = 'update "{table}" set "{col_name}"="{value}"\
+                where "{primary_key}"="{id}"'.format(\
+                table=table_name,\
+                col_name=cur_browser.get_col_name(),\
+                value=cmd[2],\
+                primary_key=cur_browser.PRIMARY_KEY,\
+                id=cur_browser.get_cur_rowid())
+        cur_db.execute(s)
+        cur_db.commit()
+        cur_browser.update_cur_cell()

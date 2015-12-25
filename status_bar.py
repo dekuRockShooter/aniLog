212badc1557767e9488164e8975353967c77e470
dekuRockShooter <dekuRockShooter@gmail.com>
2015-12-25 10:11:54 -0800

    Implement create, update, and on_browser_switch.

--- status_bar.py
+++ status_bar.py
@@ -1,5 +1,6 @@
 import curses
 import curses.textpad
+import shared
 
 # TODO: no hard coding
 class StatusBar:
@@ -16,12 +17,24 @@ class StatusBar:
         self._cur_str = new_str
 
     def create(self):
-        pass
+        cur_browser = shared.BrowserFactory.get_cur()
+        name = cur_browser.get_name()
+        idx = shared.BrowserFactory.get_cur_idx() + 1
+        browser_count = shared.BrowserFactory.get_count()
+        self._cur_str = '{}:{}/{}'.format(name, idx, browser_count)
+
+    def update(self):
+        """Redisplays itself using updated information."""
+        self._win.addstr(0,0, ''.join([' ' for i in range(79)]))#\
+        self.create()
+        self._win.addstr(0, 0, self._cur_str)
+        self._win.refresh()
 
     def destroy(self):
         curses.echo()
 
     def redraw(self):
+        self._win.addstr(0,0, ''.join([' ' for i in range(79)]))#\
         self._win.addstr(0, 0, self._cur_str)
         self._win.refresh()
         txt = self._text_pad.edit()
@@ -60,9 +73,21 @@ class StatusBar:
             # cmd_map[parsed_str[0]](parsed_str[1:]*)
             pass
         self._win.addstr(0,0, ''.join([' ' for i in range(79)]))#\
-                #['x' for i in range(self._scr_right_col - 1)]))
-        self._win.refresh()
+        self.update()
         return parsed_str
 
     def scroll(self, direction, quantifier=1):
         pass
+
+    def on_browser_switch(self):
+        """ Switch to the new browser and display it.
+
+        Assumes that the current browser has already had its create method
+        called.
+        """
+        cur_browser = shared.BrowserFactory.get_cur()
+        name = cur_browser.get_name()
+        idx = shared.BrowserFactory.get_cur_idx() + 1
+        browser_count = shared.BrowserFactory.get_count()
+        self._cur_str = '{}:{}/{}'.format(name, idx, browser_count)
+        self.update()

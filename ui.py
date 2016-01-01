00d6e8be3e63272f95359abba016c20682a471df
dekuRockShooter <dekuRockShooter@gmail.com>
2016-01-01 09:56:01 -0800

    Handle widget positioning and creation.
    The UI is meant to display widgets.  This commit makes the UI create and
    place the widgets to display.

--- ui.py
+++ ui.py
@@ -1,11 +1,8 @@
 import curses
 import os
-import keymap
-import commands
 import settings
 from browser import Browser
 from shared import BrowserFactory, DBRegistry, StatusBarRegistry
-from keymap import KeyMap
 
 # TODO: merge in aniLog.py
 class UI:
@@ -36,6 +33,43 @@ class UI:
         curses.cbreak()
         curses.noecho()
         curses.curs_set(0)
+        self._set_coords()
+        self._create_widgets()
+
+    def _create_widgets(self):
+        tables = [('watching'), ('backlog'), ('completed')]
+        b1 = BrowserFactory.create(settings.BROWSER_UPPER_LEFT_COORDS,
+                                   settings.BROWSER_BOTTOM_RIGHT_COORDS,
+                                   settings.COL_WIDTHS,
+                                   settings.DEFAULT_DB_NAME,
+                                   tables[0])
+        b2 = BrowserFactory.create(settings.BROWSER_UPPER_LEFT_COORDS,
+                                   settings.BROWSER_BOTTOM_RIGHT_COORDS,
+                                   settings.COL_WIDTHS,
+                                   settings.DEFAULT_DB_NAME,
+                                  'backlog')
+        b3 = BrowserFactory.create(settings.BROWSER_UPPER_LEFT_COORDS,
+                                   settings.BROWSER_BOTTOM_RIGHT_COORDS,
+                                   settings.COL_WIDTHS,
+                                   settings.DEFAULT_DB_NAME,
+                                   'completed')
+        b1.create()
+        b2.create()
+        b3.create()
+        StatusBarRegistry.create(1, settings.cmd_map).update()
+
+    def _set_coords(self):
+        curses.update_lines_cols()
+        if settings.STATUS_BAR_POSITION == settings.SCREEN_TOP:
+            settings.STATUS_BAR_COORDS = (0, 0)
+            settings.BROWSER_UPPER_LEFT_COORDS = (1, 0)
+            settings.BROWSER_BOTTOM_RIGHT_COORDS = (curses.LINES - 1,
+                                                    curses.COLS - 1)
+        else:
+            settings.STATUS_BAR_COORDS = (curses.LINES - 1, 0)
+            settings.BROWSER_UPPER_LEFT_COORDS = (0, 0)
+            settings.BROWSER_BOTTOM_RIGHT_COORDS = (curses.LINES - 2,
+                                                    curses.COLS - 1)
 
     def destroy(self):
         """Destroy all object and end curses."""
@@ -74,11 +108,9 @@ class UI:
                 except KeyError:
                     cmd = None
             elif key == curses.KEY_RESIZE:
-                curses.update_lines_cols()
-                BrowserFactory.get_cur().on_screen_resize(
-                        curses.LINES, curses.COLS)
-                StatusBarRegistry.get().on_screen_resize(
-                        curses.LINES, curses.COLS)
+                self._set_coords()
+                BrowserFactory.get_cur().on_screen_resize()
+                StatusBarRegistry.get().on_screen_resize()
                 continue
             else:
                 try:

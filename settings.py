c78b8d169afb372f4089d0d153de63758079a122
dekuRockShooter <dekuRockShooter@gmail.com>
2016-01-01 13:35:08 -0800

    Rearrange keys.

--- settings.py
+++ settings.py
@@ -18,6 +18,8 @@ cmd_map = {
     'new_entry': commands.NewEntry('new_entry', 'Insert a new entry.'),
     'del_entry': commands.DeleteEntry('del_entry', 'Delete an entry.'),
     'sort': commands.Sort('del_entry', 'Delete an entry.'),
+    'next_browser': commands.NextBrowser('del_entry', 'Delete an entry.'),
+    'prev_browser': commands.PreviousBrowser('del_entry', 'Delete an entry.'),
     }
 # Keymap settings
 key_map.add_key('k',commands.ScrollUp('scroll_up', 'Scroll one line up.'))
@@ -27,19 +29,15 @@ key_map.add_key('h',commands.ScrollLeft('scroll_left',
     'Scroll one column left.'))
 key_map.add_key('l',commands.ScrollRight('scroll_right',
     'Scroll one column right,'))
-key_map.add_key('i',commands.NewEntry('new_entry', 'Create a new entry'))
+key_map.add_key('i', cmd_map['new_entry'])
 key_map.add_key('y',commands.CopyEntry('copy_entry', 'Copy an entry'))
 key_map.add_key('p',commands.PasteEntry('paste_entry', 'Paste an entry'))
-key_map.add_key('gt',commands.NextBrowser('next_browser',
-    'Go to the next browser'))
-key_map.add_key('gT',commands.PreviousBrowser('previous_browser',
-    'Go to the previous browser'))
-key_map.add_key('sa',commands.Sort('sort_asc', 
-    'Sort in ascending order based on the current column.',
-    direction=commands.Sort.ASC))
-key_map.add_key('sd',commands.Sort('sort_dec',
-    'Sort in descending order based on the current column.',
-    direction=commands.Sort.DES))
+key_map.add_key('gt', cmd_map['next_browser'])
+key_map.add_key('gT', cmd_map['prev_browser'])
+#key_map.add_key('sa', cmd_map['sort_asc'])
+#key_map.add_key('sd',commands.Sort('sort_dec',
+    #'Sort in descending order based on the current column.',
+    #direction=commands.Sort.DES))
 key_map.add_key(':',commands.Write('', 'open_console',
     'Open console for editing.'))
 key_map.add_key('c',commands.Write('edit %p %v', 'edit_cell',

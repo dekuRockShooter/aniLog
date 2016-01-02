3538f02ff426d584a1936a619a780b2cff4e5471
dekuRockShooter <dekuRockShooter@gmail.com>
2016-01-01 16:13:28 -0800

    Create a package for housing settings.

---
+++ settings/positions.py
@@ -0,0 +1,7 @@
+DEFAULT_DB_NAME = 'Sybil.db'
+SCREEN_TOP = 1
+SCREEN_BOTTOM = 2
+# Load settings
+# Sizes and position settings
+STATUS_BAR_POSITION = SCREEN_BOTTOM
+COL_WIDTHS = [3,20,4,4,10,10,3,10,20]
